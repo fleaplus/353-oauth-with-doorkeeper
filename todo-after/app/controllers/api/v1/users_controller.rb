8282b6026014d410f97ee532cece34734ab367d6
Geoffrey Thompson <fleaplus@gmail.com>
2016-02-08 11:39:40 -0700

    Add Cross-origin support.

--- todo-after/app/controllers/api/v1/users_controller.rb
+++ todo-after/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,21 @@
 module Api
   module V1
     class UsersController < BaseController
+      # protect_from_forgery with: :null_session
+      before_filter :allow_cors
+      skip_before_filter :verify_authenticity_token
+
+      # For all responses in this controller, return the CORS access control headers.
+
+      def allow_cors
+        headers["Access-Control-Allow-Origin"] = "*"
+        headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+        headers["Access-Control-Allow-Headers"] =
+          %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token X-AuthToken X-Identity Authorization}.join(",")
+
+        render text: '' if request.request_method == "OPTIONS"
+      end
+
       doorkeeper_for :all
       respond_to :json
 
